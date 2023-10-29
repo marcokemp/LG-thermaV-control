6d6b82a711bb40ff829fd59f22eebf1f072ff417
marcokemp <77441991+marcokemp@users.noreply.github.com>
2023-10-29 13:16:00 +0100

    Update custom_marco.h
    - turn pump on less during non-active (5min/2hrs)
    - added manual cooling behavior (in IDLE state) via home-assistant-switch

--- customize/custom_marco.h
+++ customize/custom_marco.h
@@ -6,20 +6,27 @@ Customize::Customize()
     max_stooklijn_correction_pos = 3.0;
     max_stooklijn_correction_neg = -5.0;
     base_stooklijn_flow = 30.0;
-}
 
+}
 
 void Customize::custom_idle_behavior() {
     //implemented pump runs intermittently during idling
     
     static bool pumptimer_switch = false;
     static int pumptimer = 0;
+    if(id(cooling_switch_number).state==1){
+        ESP_LOGD("cooling or heating", "cooling on, resetting pumptimer for idle behavior");
+        pumptimer = 360;
+    }
+    if(id(cooling_switch_number).state==0){
+        ESP_LOGD("cooling or heating", "cooling off, normal idle behavior");
+    }
     if (pumptimer==0) {
-        pumptimer = 45;
+        pumptimer = 360;
         pumptimer_switch = true;
     } else if(pumptimer<15) {
         if (!pumptimer_switch) {
-            ESP_LOGD("pumptimer", "pump on for 5 minutes to circulate water");
+            ESP_LOGD("pumptimer", "pump on for 5 minutes to circulate water & run pump");
             pumptimer_switch = true;
             id(modbus_enable_heat).turn_on();
         }
@@ -29,7 +36,7 @@ void Customize::custom_idle_behavior() {
     } else {
         if(pumptimer_switch) {
             pumptimer_switch=false;
-            ESP_LOGD("pumptimer", "pump off for 10 minutes to save energy");
+            ESP_LOGD("pumptimer", "pump off for 1h55 minutes to save energy");
             id(modbus_enable_heat).turn_off();
         }
         //ESP_LOGD("pumptimer", "pump off");
