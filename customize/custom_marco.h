6429fb0c1fcd7043e7d7abd9e21d5b3ee3915084
marcokemp <77441991+marcokemp@users.noreply.github.com>
2023-11-13 20:29:24 +0100

    Update custom_marco.h
    temp fix custom idle behavior so pump is immediately turned off when going to idle instead of after 1 wait-cycle

--- customize/custom_marco.h
+++ customize/custom_marco.h
@@ -12,33 +12,39 @@ Customize::Customize()
 void Customize::custom_idle_behavior() {
     //implemented pump runs intermittently during idling
     
-    static bool pumptimer_switch = false;
+    //static bool pumptimer_switch = false;
     static int pumptimer = 0;
-    if(id(cooling_switch_number).state==1){
-        ESP_LOGD("cooling or heating", "cooling on, resetting pumptimer for idle behavior");
-        pumptimer = 4320;
-    }
-    if(id(cooling_switch_number).state==0){
-        ESP_LOGD("cooling or heating", "cooling off, normal idle behavior");
-    }
+    //replace cooling switch behavior by statemachine DL
+    //if(id(cooling_switch_number).state==1){
+    //    ESP_LOGD("cooling or heating", "cooling on, resetting pumptimer for idle behavior");
+    //    pumptimer = 4320;
+    //}
+    //if(id(cooling_switch_number).state==0){
+    //    ESP_LOGD("cooling or heating", "cooling off, normal idle behavior");
+    //
+    
     if (pumptimer==0) {
         pumptimer = 4320;
-        pumptimer_switch = true;
+        //pumptimer_switch = true;
     } else if(pumptimer<15) {
-        if (!pumptimer_switch) {
+        //if (!pumptimer_switch) {
+        //    pumptimer_switch = true;
             ESP_LOGD("pumptimer", "pump on for 5 minutes to circulate water & run pump");
-            pumptimer_switch = true;
-            id(modbus_enable_heat).turn_on();
-        }
+            if(!id(modbus_enable_heat).state){
+                id(modbus_enable_heat).turn_on();
+            }
+        //}
         //ESP_LOGD("pumptimer", "pump on");
         //id(modbus_enable_heat).turn_on();
         pumptimer--;
     } else {
-        if(pumptimer_switch) {
-            pumptimer_switch=false;
+        //if(pumptimer_switch) {
+        //    pumptimer_switch=false;
             ESP_LOGD("pumptimer", "pump off for 23h55 minutes to save energy");
-            id(modbus_enable_heat).turn_off();
-        }
+            if(id(modbus_enable_heat).state){
+                id(modbus_enable_heat).turn_off();
+            }
+        //}
         //ESP_LOGD("pumptimer", "pump off");
         //id(modbus_enable_heat).turn_off();
         pumptimer--;
